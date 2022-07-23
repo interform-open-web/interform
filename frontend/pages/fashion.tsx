199a5a0278df9c4ea202ce949989fc4b66061aa6
straightupjac <jdc.jaclyn@gmail.com>
2022-07-24 01:42:28 +0200

    add mock data

---
+++ frontend/pages/fashion.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next"
+import FashionFormData from '../../mocks/fashion-form.json';
+
+const FashionForm: NextPage = () => {
+  console.log(FashionFormData)
+  return (
+    <></>
+  )
+}
+
+export default FashionForm
