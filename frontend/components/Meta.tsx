6cebb67f12a9684beacf7d8dc2221579d69f4a28
straightupjac <jdc.jaclyn@gmail.com>
2022-07-24 01:28:54 +0200

    basic page

---
+++ frontend/components/Meta.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head"
+
+export const Meta = () => {
+  return (
+    <Head>
+      <title>Interform Demo</title>
+      <meta name="title" content="interform demo" />
+      <meta name="description" content="Forms on the dentralized web. Powered by InterPlanetary File System and Filecoin." />
+      <link rel="icon" href="/favicon.ico" />
+      <meta property="og:type" content="website" />
+      <meta name="og:title" content="Interform Demo" />
+      <meta name="og:description" content="Forms on the dentralized web. Powered by InterPlanetary File System and Filecoin." />
+    </Head>
+  )
+}
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,45 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { Meta } from '../components/Meta'
+
+
+/* Theming */
+const theme = extendTheme({
+  styles: {
+    global: {
+      a: {
+        _hover: {
+          textDecoration: 'underline',
+        },
+      },
+      h1: {
+        fontSize: '4xl',
+        fontWeight: 'bold',
+      },
+      h2: {
+        fontSize: '2xl',
+        fontWeight: 'bold',
+      },
+      h3: {
+        fontSize: 'lg'
+      },
+      h4: {
+        fontSize: 'md'
+      },
+    },
+  },
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Meta />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  )
 }
 
 export default MyApp
