a7193600cd0ef61fd14461c9eb0ba37ad09738b7
straightupjac <jdc.jaclyn@gmail.com>
2022-07-24 09:10:34 -0700

    backlink to landing page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, IconButton, Text, VStack } from '@chakra-ui/react'
+import { Button, HStack, IconButton, Text, VStack } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import { FaGithub } from 'react-icons/fa'
 import styles from '@styles/Home.module.css'
@@ -16,16 +16,21 @@ const Home: NextPage = () => {
             <Text fontSize='1.5rem'>
               {`Forms on the dentralized web. Powered by InterPlanetary File System and Filecoin.`}
             </Text>
-            <Link href="/builder">
-              <Button>Build your own form</Button>
-            </Link>
+            <HStack gap={2}>
+              <Link href="/builder">
+                <Button>Build your own form</Button>
+              </Link>
+              <Link href="https://www.interform.app/">
+                <Button>Landing Page</Button>
+              </Link>
+            </HStack>
             <p className={styles.description}>
               Check out the source code{' '}
-              <a href="https://github.com/straightupjac/interform" target="_blank" rel="noreferrer">
+              <a href="https://github.com/interform-open-web/interform" target="_blank" rel="noreferrer">
                 <code className={styles.code}>interform</code>
               </a>
             </p>
-            <a href="https://github.com/straightupjac/interform" rel="noreferrer" target="_blank" >
+            <a href="https://github.com/interform-open-web/interform" rel="noreferrer" target="_blank" >
               <IconButton
                 aria-label="github icon"
                 colorScheme="dark"
