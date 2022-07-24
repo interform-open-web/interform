3c5706c14bd5953dc48b2133a1b5dfe936e7824c
straightupjac <jdc.jaclyn@gmail.com>
2022-07-24 10:46:15 -0700

    fake form

---
+++ frontend/pages/bafybeig66nby6tiaz27ph67nsuxzuhn2a6fpx6hqpudttm3aaku3x7hvwm.tsx
@@ -0,0 +1,23 @@
+import { Center, Container, Text, VStack } from "@chakra-ui/react";
+import { LoadedForm } from "@components/LoadedForm";
+import { NextPage } from "next"
+import FormData from '../mocks/open-web-form.json';
+
+const Censor: NextPage = () => {
+  const formStructure = FormData;
+  const dateCreated = new Date(formStructure.timestamp);
+
+  return (
+    <Container paddingTop={20} paddingBottom={10} maxWidth={'1500px'}>
+      <Center>
+        <VStack gap={2}>
+          <h1>{formStructure.name}</h1>
+          <Text size="12px">Created: {dateCreated.toLocaleDateString()} {dateCreated.toLocaleTimeString()}</Text>
+          <LoadedForm formSchema={formStructure.formOptions} />
+        </VStack>
+      </Center>
+    </Container>
+  )
+}
+
+export default Censor
