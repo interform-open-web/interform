c9aecccb37cce33146105207633dcb681a5edf66
0xgoretex <0xamen@protonmail.com>
2022-07-24 12:16:08 -0400

    added readme + minor fix

--- frontend/pages/api/forms.ts
+++ frontend/pages/api/forms.ts
@@ -33,6 +33,7 @@ export default async function handler(
       result: {
         cid: formCid,
       },
+      message: "Form added successfully",
     })
   } else {
     res.status(400).json("Unable to handle request");
